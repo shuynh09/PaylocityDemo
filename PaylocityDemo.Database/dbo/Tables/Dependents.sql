7eb7885a3d6f5c0b6935a6a5fa8a7c431056e671
Sang Huynh <shuynh09@outlook.com>
2020-03-03 13:51:18 -0700

    Add database project to solution

---
+++ PaylocityDemo.Database/dbo/Tables/Dependents.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Dependents] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [FullName]     NVARCHAR (MAX) NULL,
+    [ModifiedDate] DATETIME2 (7)  NOT NULL,
+    [EmployeeId]   INT            NULL,
+    CONSTRAINT [PK_Dependents] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Dependents_Employees_EmployeeId] FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employees] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Dependents_EmployeeId]
+    ON [dbo].[Dependents]([EmployeeId] ASC);
+
---
+++ PaylocityDemo.Database/dbo/Tables/Employees.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [FullName]     NVARCHAR (MAX) NULL,
+    [ModifiedDate] DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
